c43237538757771fa59063f89e7ddb77ca3bdc98
jumper385 <henrychen385@gmail.com>
2020-07-27 11:25:44 +0800

    api root test requests

--- web/src/routes/apiv1/index.js
+++ web/src/routes/apiv1/index.js
@@ -1,3 +1,34 @@
 export const get = (req,res) => {
-    res.json('hello world')
+
+    // A function that runs every time you hit this endpoint
+    const sampleEmoji = () => {
+        let emojis = ['🦐','🍰','😴','🙄','🎀','🍬','🎳','🩰','🥯'];
+        let index = Math.floor(Math.random()*(emojis.length));
+        return emojis[index]
+    }
+
+    // Response of the GET request
+    res.json({
+        message: `GET request`,
+        payload: {
+            timestamp: new Date(),
+            random_emoji: sampleEmoji()
+        }
+    });
+};
+
+export const post = (req,res) => {
+    // return the request body
+    res.json({
+        message: `POST request`,
+        payload: req.body
+    });
+};
+
+export const put = (req,res) => {
+    // return the request body
+    res.json({
+        message: `PUT request`,
+        payload: req.body
+    });
 }
--- web/src/server.js
+++ web/src/server.js
@@ -2,6 +2,7 @@ import sirv from 'sirv';
 import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import * as bodyParser from 'body-parser'
 
 const app = express();
 
@@ -10,6 +11,8 @@ const dev = NODE_ENV === 'development';
 
 app.use(compression({ threshold: 0 }));
 app.use(sirv('static', { dev }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(sapper.middleware());
 
 app.listen(PORT, () => {
