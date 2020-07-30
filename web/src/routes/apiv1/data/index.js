8e8b8103c389902a02f70478e7e61f71d7b02b39
carrrmenleong <carmen020824@gmail.com>
2020-07-30 10:34:12 +0800

    created put, del, get request

--- web/src/routes/apiv1/data/index.js
+++ web/src/routes/apiv1/data/index.js
@@ -1,7 +1,31 @@
 export function post(req,res){
-    // return the request body
+    // return the location and timestamp of the datapoint sent
     var {location,moduleID,timestamp} = req.body;
     res.json({
         message: `You sent a datapoint from ${location} at ${timestamp}`
+    });
+};
+
+export function put(req,res){
+    // return the type of request and timestamp
+    res.json({
+        message: `This is a PUT request`,
+        timestamp: new Date()
+    });
+};
+
+export function del(req,res){
+    // return the type of request and timestamp
+        res.json({
+        message: `This is a DELETE request`,
+        timestamp: new Date()
+    });
+};
+
+export function get(req,res){
+    // return the type of request and timestamp
+        res.json({
+        message: `This is a GET request`,
+        timestamp: new Date()
     });
 };
